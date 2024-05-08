e90fd225c486be2822ed81b4ee2a4fd71cb62efe
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-05-08 17:45:25 +0700

    settings

--- Bai2/obj/Debug/net8.0/Bai2.AssemblyInfo.cs
+++ Bai2/obj/Debug/net8.0/Bai2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388571992bf9f2e3db5e21d8ad6fc9759bd45c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94abf7d47fba9433b898dd2bb9317f4cdda317a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
